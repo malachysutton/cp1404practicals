dd59e6cbb0c6ec7a6e4c250b4041f18bc8983426
Malachy <linden.mrman@gmail.com>
2023-10-11 21:20:51 +1000

    Imported random and used function

---
+++ prac_02/randoms.py
@@ -0,0 +1,9 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+#on line one number 6
+#on line two number 7
+#on line three number 4.300246401780387
