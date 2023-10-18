42cf7fe8af2446fb1ab4653416d14c045db72666
Malachy <linden.mrman@gmail.com>
2023-10-18 18:06:10 +1000

    Created similar program for the delete function to test

--- prac_04/do_this_now_lecture4.py
+++ prac_04/do_this_now_lecture4.py
@@ -1,3 +1,8 @@
+# names = ["tom", "bob", "dave", "steve", "tim"]
+# number_wanted = int(input("Which number name would you like? "))
+# print(names[number_wanted])
+
 names = ["tom", "bob", "dave", "steve", "tim"]
-number_wanted = int(input("Which number name would you like? "))
-print(names[number_wanted])
+number_delete = int(input("Which number name would you like? "))
+del names[number_delete]
+print([names])
