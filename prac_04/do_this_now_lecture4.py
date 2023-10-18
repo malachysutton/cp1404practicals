42478dbfbb0d5ca99002ee3b45a574ab9eb762ec
Malachy <linden.mrman@gmail.com>
2023-10-18 18:09:32 +1000

    Created similar program for appending a list to add an extra name to the list

--- prac_04/do_this_now_lecture4.py
+++ prac_04/do_this_now_lecture4.py
@@ -2,7 +2,12 @@
 # number_wanted = int(input("Which number name would you like? "))
 # print(names[number_wanted])
 
+# names = ["tom", "bob", "dave", "steve", "tim"]
+# number_delete = int(input("Which number name would you like? "))
+# del names[number_delete]
+# print([names])
+
 names = ["tom", "bob", "dave", "steve", "tim"]
-number_delete = int(input("Which number name would you like? "))
-del names[number_delete]
+name_added = input("Which name would you like added? ")
+names.append(name_added)
 print([names])
