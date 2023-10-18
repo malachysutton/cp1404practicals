0ac8526ac4c86df8ca95ec8f0316fe3a02858a8b
Malachy <linden.mrman@gmail.com>
2023-10-18 11:00:48 +1000

    Attempting to multiply sequence

--- prac_03/numbers.py
+++ prac_03/numbers.py
@@ -1,6 +1,6 @@
 number_file = open("numbers.txt", "r")
 first_number=number_file.readline(1)
 second_number=number_file.readline(2)
-result = first_number * second_number
+result =
 print(result)
 number_file.close()
